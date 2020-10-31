b51174812bc50363f85205d867ea9f7fd8ae1adc
gildorsj <gildorsj@gmail.com>
2020-10-30 22:28:24 -0300

    Add files via upload

--- projetoFinal.ino
+++ projetoFinal.ino
@@ -6,232 +6,322 @@
 #define RST_PIN 9
 
 #define RELE 4
-#define LED_AMARELO 5
-#define LED_VERDE 6
-#define LED_VERMELHO 7
-//Led
-int red_light_pin = 4;
-int green_light_pin = 3;
-int blue_light_pin = 2;
+#define BLUE_LIGHT 5
+#define GREEN_LIGHT 6
+#define RED_LIGHT 7
+
+#define EEPROMSpace 10
 
 MFRC522 rfidModule(SS_PIN, RST_PIN);
 
-String registeredTag[10] = {"5bd38a1b", "0", "0", "0", "0", "0", "0", "0", "694989bb", "0"};
 String tag = "";
 int counter = 0;
 bool access = false;
 
+String getTag(int index);
+void putTag(int index, String tag);
+void configurationMode();
+void eraseTags();
+void eraseAll();
+void eraseMode();
+void eraseAllMode();
+void scanMode();
+void accessGranted();
+void accessDenid();
+void actionEraseMode();
+void actionErasAllMode();
+void actionScanMode();
+void actionConfigurationMode();
+void actionConfirmed();
+void actionAccessGranted();
+void ActionAccessDenid();
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value);
+
 void setup()
 {
-  Serial.begin(9600);
-  SPI.begin();
-  rfidModule.PCD_Init();
-  pinMode(RELE, OUTPUT);
+
+   //Limpeza da EEPROM (Executado apenas para limpar a memoria)
+   for (int i = 0; i < EEPROMSpace * 8 ; i++) {
+       EEPROM.write(i, 0);
+   }
+
+   Serial.begin(9600);
+   SPI.begin();
+   rfidModule.PCD_Init();
+   pinMode(BLUE_LIGHT, OUTPUT);
+   pinMode(GREEN_LIGHT, OUTPUT);
+   pinMode(RED_LIGHT, OUTPUT);
+   pinMode(RELE, OUTPUT);
 }
 
 void loop()
 {
-  if (counter == 3)
-    configurationMode();
-  else
-    scanMode();
+   switch (counter)
+   {
+   case 3:
+      configurationMode();
+      break;
+   case 6:
+      eraseMode();
+      break;
+   case 10:
+      eraseAllMode();
+      break;
+   default:
+      scanMode();
+   }
+}
 
-  if (counter == 6)
-    eraseMode();
+String getTag(int index)
+{
+   String tag = "";
+   for (int i = index * 8; i < index * 8 + 8; i++)
+      tag.concat(char(EEPROM.read(i)));
+   return tag;
+}
+
+void putTag(int index, String tag)
+{
+   for (int i = index, j = 0; i < index + 8; i++, j++)
+      EEPROM.write(i, tag[j]);
 }
 
 void configurationMode()
 {
-  actionConfigurationMode();
+   actionConfigurationMode();
+   delay(1000);
 
-  if (!rfidModule.PICC_IsNewCardPresent() || !rfidModule.PICC_ReadCardSerial())
-  {
-    delay(50);
-    return;
-  }
-
-  tag = "";
-  for (byte i = 0; i < rfidModule.uid.size; i++)
-    tag.concat(String(rfidModule.uid.uidByte[i], HEX));
-
-  if (tag.equalsIgnoreCase(registeredTag[0]))
-  {
-    counter++;
-    return 0;
-  }
-
-  for (int i = 1; i < (sizeof(registeredTag) / sizeof(String)); i++)
-  {
-    if (tag.equalsIgnoreCase(registeredTag[i]))
-    {
-      counter = 0;
+   if (!rfidModule.PICC_IsNewCardPresent() || !rfidModule.PICC_ReadCardSerial())
+   {
+      delay(50);
       return;
-    }
-  }
+   }
 
-  for (int i = 1; i < (sizeof(registeredTag) / sizeof(String)); i++)
-  {
-    if (registeredTag[i] == "0")
-    {
-      registeredTag[i] = tag;
-      actionNewTagRegistered();
-      counter = 0;
-      Serial.println("New id registered: " + tag);
-      return;
-    }
-  }
+   tag = "";
+   for (byte i = 0; i < rfidModule.uid.size; i++)
+      tag.concat(String(rfidModule.uid.uidByte[i], HEX));
+
+   if (tag.equalsIgnoreCase(getTag(0)))
+   {
+      counter++;
+      return 0;
+   }
+
+   for (int i = 1; i < EEPROMSpace; i++)
+   {
+      if (tag.equalsIgnoreCase(getTag(i)))
+      {
+         counter = 0;
+         return;
+      }
+   }
+
+   for (int i = 0; i < EEPROM.length(); i = i + 8)
+   {
+      if (EEPROM.read(i) == 0)
+      {
+         putTag(i, tag);
+         actionConfirmed();
+         counter = 0;
+         Serial.println("New id registered: " + tag);
+         return;
+      }
+   }
 }
 
 void eraseTags()
 {
-  for (int i = 1; i < (sizeof(registeredTag) / sizeof(String)); i++)
-  {
-    registeredTag[i] = "0";
-  }
+   for (int i = 8; i < EEPROMSpace * 8; i++)
+   {
+      EEPROM.write(i, 0);
+   }
+}
+
+void eraseAll()
+{
+   for (int i = 0; i < EEPROMSpace * 8; i++)
+   {
+      EEPROM.write(i, 0);
+   }
 }
 
 void eraseMode()
 {
-  actionEraseMode();
+   delay(1000);
+   actionEraseMode();
 
-  if (!rfidModule.PICC_IsNewCardPresent() || !rfidModule.PICC_ReadCardSerial())
-  {
-    delay(50);
-    return;
-  }
+   if (!rfidModule.PICC_IsNewCardPresent() || !rfidModule.PICC_ReadCardSerial())
+   {
+      delay(50);
+      return;
+   }
 
-  tag = "";
-  for (byte i = 0; i < rfidModule.uid.size; i++)
-    tag.concat(String(rfidModule.uid.uidByte[i], HEX));
+   tag = "";
+   for (byte i = 0; i < rfidModule.uid.size; i++)
+      tag.concat(String(rfidModule.uid.uidByte[i], HEX));
 
-  if (tag.equalsIgnoreCase(registeredTag[0]))
-  {
-    Serial.println("Exit erase mode");
-    counter = 0;
-    return;
-  }
-  else
-  {
-    for (int i = 1; i < (sizeof(registeredTag) / sizeof(String)); i++)
-    {
-      if (tag.equalsIgnoreCase(registeredTag[i]))
+   if (tag.equalsIgnoreCase(getTag(0)))
+   {
+      counter++;
+      return 0;
+      Serial.println("Exit erase tag mode");
+   }
+
+   for (int i = 1; i < EEPROMSpace; i++)
+   {
+      if (tag.equalsIgnoreCase(getTag(i)))
       {
-        eraseTags();
-        counter = 0;
-        Serial.println("All tags erased");
-        return;
+         eraseTags();
+         actionConfirmed();
+         Serial.println("Tags erased.");
+         counter = 0;
+         return;
       }
-    }
-  }
+   }
+}
+
+void eraseAllMode()
+{
+   delay(1000);
+   actionErasAllMode();
+
+   if (!rfidModule.PICC_IsNewCardPresent() || !rfidModule.PICC_ReadCardSerial())
+   {
+      delay(50);
+      return;
+   }
+
+   tag = "";
+   for (byte i = 0; i < rfidModule.uid.size; i++)
+      tag.concat(String(rfidModule.uid.uidByte[i], HEX));
+
+   if (tag.equalsIgnoreCase(getTag(0)))
+   {
+      Serial.println("Exit erase all mode");
+      counter = 0;
+      return;
+   }
+   else
+   {
+      for (int i = 0; i < EEPROMSpace; i++)
+      {
+         if (tag.equalsIgnoreCase(getTag(i)))
+         {
+            eraseAll();
+            actionConfirmed();
+            counter = 0;
+            Serial.println("All tags erased");
+            return;
+         }
+      }
+   }
 }
 
 void scanMode()
 {
-  actionScanMode();
+   actionScanMode();
 
-  if (registeredTag[0] == "0")
-  {
-    configurationMode();
-    counter = 3;
-  }
+   if (EEPROM.read(0) == 0)
+   {
+      counter = 3;
+   }
 
-  if (!rfidModule.PICC_IsNewCardPresent() || !rfidModule.PICC_ReadCardSerial())
-    return;
+   if (!rfidModule.PICC_IsNewCardPresent() || !rfidModule.PICC_ReadCardSerial())
+      return;
 
-  tag = "";
-  for (byte i = 0; i < rfidModule.uid.size; i++)
-    tag.concat(String(rfidModule.uid.uidByte[i], HEX));
+   tag = "";
+   for (byte i = 0; i < rfidModule.uid.size; i++)
+      tag.concat(String(rfidModule.uid.uidByte[i], HEX));
 
-  if (tag.equalsIgnoreCase(registeredTag[0]))
-    counter++;
-  else
-    counter = 0;
+   if (tag.equalsIgnoreCase(getTag(0)))
+      counter++;
+   else
+      counter = 0;
 
-  for (int i = 0; i < (sizeof(registeredTag) / sizeof(String)); i++)
-  {
-    if (tag.equalsIgnoreCase(registeredTag[i]))
-      access = true;
-  }
+   for (int i = 0; i < EEPROMSpace; i++)
+   {
+      if (tag.equalsIgnoreCase(getTag(i)))
+         access = true;
+   }
 
-  Serial.println("Counert: " + counter);
+   if (access == true)
+      accessGranted();
+   else
+      accessDenid();
 
-  if (access == true)
-    accessGranted();
-  else
-    accessDenid();
-
-  delay(1000);
+   delay(1000);
 }
 
 void accessGranted()
 {
-  Serial.println("Access Granted id: " + tag);
-  actionAccessGranted();
-  access = false;
+   Serial.println("Access Granted id: " + tag);
+   actionAccessGranted();
+   access = false;
 }
 
 void accessDenid()
 {
-  Serial.println("Access Denid id: " + tag);
-  ActionAccessDenid();
+   Serial.println("Access Denid id: " + tag);
+   ActionAccessDenid();
+}
+
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+{
+   analogWrite(RED_LIGHT, red_light_value);
+   analogWrite(GREEN_LIGHT, green_light_value);
+   analogWrite(BLUE_LIGHT, blue_light_value);
 }
 
 void actionEraseMode()
 {
-  RGB_color(0, 0, 0);
-  RGB_color(255, 0, 0);
-  delay(250);
-  RGB_color(0, 0, 0);
-  delay(250);
+   RGB_color(255, 255, 0);
+   delay(400);
+   RGB_color(0, 0, 0);
 }
-
+void actionErasAllMode()
+{
+   RGB_color(255, 0, 0);
+   delay(400);
+   RGB_color(0, 0, 0);
+}
 void actionScanMode()
 {
-  RGB_color(255, 0, 255);
+   RGB_color(255, 255, 0);
 }
 
 void actionConfigurationMode()
 {
-  RGB_color(0, 0, 0);
-  delay(250);
-  RGB_color(255, 0, 255);
-  delay(250);
+   RGB_color(0, 255, 0);
+   delay(400);
+   RGB_color(0, 0, 0);
 }
 
-void actionNewTagRegistered()
+void actionConfirmed()
 {
-  RGB_color(0, 255, 0);
-  delay(300);
-  RGB_color(0, 0, 0);
-  delay(300);
-  RGB_color(0, 255, 0);
-  delay(300);
-  RGB_color(0, 0, 0);
-  delay(300);
-  RGB_color(0, 255, 0);
-  delay(300);
-  RGB_color(0, 0, 0);
-  delay(500);
+   RGB_color(0, 0, 255);
+   delay(300);
+   RGB_color(0, 0, 0);
+   delay(300);
+   RGB_color(0, 0, 255);
+   delay(300);
+   RGB_color(0, 0, 0);
+   delay(300);
+   RGB_color(0, 0, 255);
+   delay(300);
+   RGB_color(0, 0, 0);
+   delay(300);
 }
 
 void actionAccessGranted()
 {
-  RGB_color(0, 0, 255);
-  digitalWrite(RELE, HIGH);
-  RGB_color(0, 0, 0);
-  delay(500);
-  RGB_color(0, 0, 0);
-  digitalWrite(RELE, LOW);
-  RGB_color(255, 0, 255);
-  delay(500);
+   RGB_color(0, 255, 0);
+   digitalWrite(RELE, HIGH);
+   delay(500);
+   digitalWrite(RELE, LOW);
 }
 
 void ActionAccessDenid()
 {
-  RGB_color(0, 0, 0);
-  RGB_color(0, 0, 0);
-  delay(500);
-  RGB_color(0, 0, 0);
-  RGB_color(255, 0, 255);
-  delay(500);
+   RGB_color(255, 0, 0);
+   delay(500);
 }
